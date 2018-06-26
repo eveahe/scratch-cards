99312c36b08b6588485e03ca2bb543223acd6a65
Glitch (smiling-blouse) <none>
2018-06-26 03:59:58 +0000

    😄🏮 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -7,11 +7,22 @@ var leftPosition;
 var clickCount = 0;
 
 $(".buttonTest").on("click", function(event){
+  //Hiding the button.
   $(event.target).hide();
-  $(".test").css("background-color", "aliceblue");
+  
+  //counting the number of clicks.
   clickCount = clickCount + 1;
-  console.log(clickCount);
   $("#clickcounttext").text("You've peeked: " + clickCount + " times.");
+  
+  //Checking where the button is, seeing if it's by the copy.
+  var x = event.clientX; // the x position of the click.
+  var y = event.clientY; // the y position of click
+  
+  if (clickCount > 5) {
+    $(".test").css("background-color", "aliceblue");
+  };
+  console.log(event.clientX + ", " + event.clientY);
+  //hardcoding in the button size for now, it's 114 x 36. 
 })
 
 
